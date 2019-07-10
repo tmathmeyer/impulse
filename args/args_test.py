59ed30cc3a0a826de8555c3a700a3771ed3a24c6
Ted Meyer <tmathmeyer@gmail.com>
2019-07-09 22:54:31 -0700

    maybe compgen is broken on alpine

--- args/args_test.py
+++ args/args_test.py
@@ -27,6 +27,7 @@ class TestDirectoryCompletion(unittest.TestCase):
     print('running test_get_directories')
     os.system('pwd')
     os.system('ls -lash')
+    os.system('compgen -o bashdefault -o default -o nospace -F _cd e')
     expanded_from_e = list(args.Directory._get_directories('e'))
     self.assertEqual(set(expanded_from_e), set(['example']))
     expanded_from_r = list(args.Directory._get_directories('r'))
