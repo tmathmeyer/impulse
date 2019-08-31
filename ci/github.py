f5496235a9427a8e04ada6c157a541c818f0ede6
Ted Meyer <tmathmeyer@google.com>
2019-08-31 16:40:19 -0700

    Better error reporting

--- ci/github.py
+++ ci/github.py
@@ -51,7 +51,13 @@ class ShellLog(object):
     self.commands = []
 
   def format_error(self):
-    return 'ERROR'
+    errmsg = 'err'
+    last_cmd = {'$?': 0}
+    if self.commands:
+      errmsg = self.commands[-1]
+      if len(self.commands) > 1:
+        last_cmd = self.commands[-2]
+    return f'{errmsg}\n\t{last_cmd}'
 
   def CMD(self, cmd) -> bool:
     result = True
@@ -87,7 +93,7 @@ class Build(api.Resource('github-pr')):
       write_this = list(self._log.commands[-1].values())[0]
       write_this = write_this.replace('\\n', '\n')
     else:
-      write_this = self._log.format_error()
+      write_this += self._log.format_error()
 
     message = (
 """
@@ -112,8 +118,8 @@ class Build(api.Resource('github-pr')):
         if not self.prepare_git_repo():
           return self.exit_msg('Unable to check out source files')
       if not os.path.exists('impulse'):
-        os.system('git clone http://192.168.0.100:10080/ted/impulse')
-      os.system('ln -s impulse/rules rules')
+        self._log.CMD(['git', 'clone', 'https://github.com/tmathmeyer/impulse'])
+      self._log.CMD(['ln', '-s', 'impulse/rules', 'rules'])
       return self._log.CMD(
         ['impulse', 'testsuite', '--debug', '--fakeroot', os.getcwd()])
 
@@ -145,7 +151,7 @@ class Build(api.Resource('github-pr')):
       return False
 
     remote_cmd = 'git remote add {} {}'.format(name, upstream)
-    pull_cmd = 'git pull {}'.format(name)
+    pull_cmd = 'git fetch {}'.format(name)
     if not self._log.CMD(remote_cmd.split()):
       return False
     if not self._log.CMD(pull_cmd.split()):
@@ -200,6 +206,12 @@ class BuildManager(api.ProvidesResources(Build)):
     self.get_updates()
     running_builds = self._builder_pool.running_build_ids()
     return [self._builds.get(i, None) for i in running_builds]
+
+    @api.METHODS.get('/ALL')
+  def get_all_build(self) -> [Build]:
+    self.get_updates()
+    running_builds = self._builder_pool.running_build_ids()
+    return list(self._builds.values())
 
   @api.METHODS.get('/<build_id>')
   def get_build(self, build_id) -> Build:
