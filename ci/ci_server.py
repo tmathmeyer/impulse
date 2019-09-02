f9e9c89374accf45b61d18951b0b5190d555beb7
Ted Meyer <tmathmeyer@google.com>
2019-09-02 00:32:39 -0400

    tracing

--- ci/ci_server.py
+++ ci/ci_server.py
@@ -1,12 +1,15 @@
 
 from impulse.hal import api
 from impulse.ci import ci_pooling
+from impulse.ci import logging
 from impulse.ci import gogs
 from impulse.ci import github
 
 def main():
   flask_app = api.GetFlask()
   builders = ci_pooling.BuilderPool(4)
+
+  logging.EnableTracing()
 
   flask_app.RegisterResourceProvider(gogs.BuildManager(builders))
   flask_app.RegisterResourceProvider(github.BuildManager(builders))
