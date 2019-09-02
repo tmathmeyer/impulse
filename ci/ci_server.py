34c18dc877acba3abf758c35e698e596257ca587
Ted Meyer <tmathmeyer@google.com>
2019-09-02 00:32:39 -0400

    tracing

--- ci/ci_server.py
+++ ci/ci_server.py
@@ -9,6 +9,8 @@ def main():
   flask_app = api.GetFlask()
   builders = ci_pooling.BuilderPool(4)
 
+  logging.EnableTracing()
+
   flask_app.RegisterResourceProvider(gogs.BuildManager(builders))
   flask_app.RegisterResourceProvider(github.BuildManager(builders))
   flask_app.run(host='0.0.0.0', port=5566)
