0cdee1b7b42475b5893b30608091a4483256228b
Ted Meyer <tmathmeyer@google.com>
2019-09-02 00:38:01 -0400

    logging by names

--- ci/logging.py
+++ ci/logging.py
@@ -1,16 +1,20 @@
 import sys
 
-def _do_trace(frame, event, arg):
-  if event == 'call':
-    func_name = frame.f_code.co_name
-    if func_name == 'write':
-      return
-    print(f'==> {func_name}')
+def _do_trace(names):
+  def __tracer__(frame, event, arg):
+    if event == 'call':
+      func_name = frame.f_code.co_name
+      if func_name == 'write':
+        return
+      if f'{frame.f_code.co_filename}.py' in names:
+        return
+      print(f'==> {func_name}')
 
-  if event in ('return', 'exception'):
-    print('ret / exc')
+    if event in ('return', 'exception'):
+      print('ret / exc')
 
-  return
+    return
+  return __tracer__
 
-def EnableTracing():
-  sys.settrace(_do_trace)
+def EnableTracing(*module_names):
+  sys.settrace(_do_trace(module_names))
