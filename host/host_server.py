08697f67c0257ec987b958bd3d5bc0f10e30789a
Ted Meyer <tmathmeyer@gmail.com>
2020-08-21 23:15:40 -0700

    keep track of running servers

--- host/host_server.py
+++ host/host_server.py
@@ -1,6 +1,7 @@
 
 import os
 import time
+import traceback
 
 import docker
 import requests
@@ -33,9 +34,10 @@ def VAR(var):
 
 
 class Host(api.Resource('host')):
-  def __init__(self, name):
+  def __init__(self, name, status='stopped'):
     super().__init__()
     self.name = name
+    self.status = status
 
   def get_id(self):
     return self.name
@@ -69,7 +71,7 @@ class ReverseProxy(Host):
 
 class FileHost(Host):
   def __init__(self, name, nginxLocations):
-    super().__init__(name)
+    super().__init__(name, status='filehost')
     self.paths = {}
     for location in nginxLocations:
       self.paths[location.location] = {
@@ -90,6 +92,10 @@ class HostManager(api.ProvidesResources(Host)):
   def get_all_build(self) -> [Host]:
     return self._nginx.Hosts()
 
+  @api.METHODS.get('/alive')
+  def get_all_build(self) -> [Host]:
+    return [h for h in self._nginx.Hosts() if h.status == 'running']
+
   @api.METHODS.get('/<hostname>')
   def byHostname(self, hostname) -> Host:
     build = self._nginx.Host(hostname)
@@ -98,12 +104,103 @@ class HostManager(api.ProvidesResources(Host)):
     raise api.ServiceError(404, 'Host not found')
 
 
+class DockerContainer(object):
+  __slots__ = ('_hostname', '_wrapped', '_port')
+  def __init__(self, container):
+    self._port = None
+    self._hostname = None
+    self._wrapped = container
+    self._setup()
+
+  def ID(self):
+    return self._wrapped.id
+
+  def Host(self):
+    return self._hostname
+
+  def _setup(self):
+    ports = self._wrapped.attrs['NetworkSettings']['Ports']
+    for boundAs, hostPort in ports.items():
+      if boundAs.endswith('/tcp') and hostPort[0]['HostIp'] == '0.0.0.0':
+        port = hostPort[0]['HostPort']
+        self._hostname = self._keepTryingNetworkRequest(
+          f'http://localhost:{port}/api/impulse/identify', timeout=2, tries=3)
+        if self._hostname is not None:
+          self._port = port
+          break
+
+    if self._hostname is None:
+      raise Warning(f'This host ({self.ID()}) is not proxyable')
+
+  def _keepTryingNetworkRequest(self, url, timeout=5, tries=1):
+    try:
+      response = requests.get(url)
+      if response.status_code != 200:
+        return None
+      return response.content.decode('utf-8')
+    except:
+      time.sleep(timeout)
+      if tries >= 1:
+        return self._keepTryingNetworkRequest(url, timeout, tries-1)
+      return None
+
+  def CommunicateNginx(self, nginx_thread_handle):
+    if self._port is None or self._hostname is None:
+      raise ValueError('DockerContainer was never initialized')
+
+    server = nginx_thread_handle.Host(self._hostname)
+    if server == None:
+      server = self._nginx_thread.CreateProxyHost(self._hostname, self._port)
+    else:
+      nginx_thread_handle.SetPort(server, self._port)
+
+
 @rpc.RPC
 class DockerThread(object):
+  __slots__ = ('_client', '_nginx_thread', '_logs',
+               '_containers_by_id', '_containers_by_host')
   def __init__(self, logs, nginx_thread_handle):
     self._client = docker.from_env()
     self._nginx_thread = nginx_thread_handle
     self._logs = logs
+    self._containers_by_id = {}
+    self._containers_by_host = {}
+    self.OnStart()
+
+  def ContainerExists(self, hostname):
+    return hostname in self._containers_by_host
+
+  def _addContainer(self, external_container):
+    try:
+      container = DockerContainer(external_container)
+      container.CommunicateNginx(self._nginx_thread)
+      if container.ID() in self._containers_by_id:
+        raise ValueError('Duplicate container ID.')
+      if container.Host() in self._containers_by_host:
+        raise ValueError('Duplicate container Hostname.')
+      self._containers_by_id[container.ID()] = container
+      self._containers_by_host[container.Host()] = container
+      self._nginx_thread.NotifyContainerHostAlive(container.Host())
+    except Exception as e:
+      self._logs.Log(traceback.format_exc())
+
+  def _removeContainer(self, external_container):
+    try:
+      container = self._containers_by_id.get(external_container.id, None)
+      if container is None:
+        self._logs.Log(f'Removing unhosted container: {external_container.id}')
+      else:
+        self._containers_by_id.pop(container.ID())
+        self._containers_by_host.pop(container.Host())
+      self._nginx_thread.NotifyContainerHostDead(container.Host())
+    except Exception as e:
+      self._logs.Log(traceback.format_exc())
+
+  def OnStart(self):
+    for container in self._client.containers.list():
+      self._addContainer(container)
+    self._logs.Log(
+      f'detected containers on start: {list(self._containers_by_host.keys())}')
     self.Listen()
 
   def Listen(self):
@@ -117,39 +214,34 @@ class DockerThread(object):
 
   # Docker event start handler
   def start(self, event):
-    self._logs.Log('Container started')
-    container = self._client.containers.get(event['id'])
-    ports = container.attrs['NetworkSettings']['Ports']
-    for boundAs, hostPort in ports.items():
-      if boundAs.endswith('/tcp') and hostPort[0]['HostIp'] == '0.0.0.0':
-        port = hostPort[0]['HostPort']
-        hostname = self._keepTryingNetworkRequest(
-          f'http://localhost:{port}/api/impulse/identify', timeout=2, tries=3)
-        if hostname == None:
-          self._logs.Log(f'Failed to query container on port {port}')
-          return
-        try:
-          server = self._nginx_thread.Host(hostname)
-          if server == None:
-            self._logs.Log(f'Creating new nginx proxy entry for {hostname}')
-            server = self._nginx_thread.CreateProxyHost(hostname, port)
-          else:
-            self._logs.Log(f'setting port for {hostname} to {port}')
-            self._nginx_thread.SetPort(server, port)
-        except Exception as e:
-          self._logs.Log(e)
+    self._logs.Log(f'container {event["id"]} started')
+    self._addContainer(self._client.containers.get(event['id']))
 
-  def _keepTryingNetworkRequest(self, url, timeout=5, tries=1):
-    try:
-      response = requests.get(url)
-      if response.status_code != 200:
-        return None
-      return response.content.decode('utf-8')
-    except:
-      time.sleep(timeout)
-      if tries >= 1:
-        return self._keepTryingNetworkRequest(url, timeout, tries-1)
-      return None
+  # Docker event stop handler
+  def stop(self, event):
+    self._logs.Log(f'container {event["id"]} stopped')
+    self._removeContainer(self._client.containers.get(event['id']))
+
+  # Docker event die handler
+  def die(self, event):
+    self._logs.Log(f'container {event["id"]} died')
+    self._removeContainer(self._client.containers.get(event['id']))
+
+  # Docker event kill handler
+  def kill(self, event):
+    pass
+
+  # Docker event attach handler
+  def attach(self, event):
+    pass
+
+  # Docker event create handler
+  def create(self, event):
+    pass
+
+  # Docker event destroy handler
+  def destroy(self, event):
+    pass
 
 
 @rpc.RPC
@@ -170,13 +262,13 @@ class NginxManagerThread(object):
         continue
 
       server_name = server.NamedProperty('server_name')
-      if server.HasProperty('root'):
-        servers[server_name] = FileHost(server_name, server.locations)
-        continue
-
       location = server.locations[0]
       if location.HasProperty('proxy_pass'):
         servers[server_name] = ReverseProxy(server_name, location)
+        continue
+
+      if server.HasProperty('root'):
+        servers[server_name] = FileHost(server_name, server.locations)
         continue
     return servers
 
@@ -210,7 +302,14 @@ class NginxManagerThread(object):
     self._logs.Log('Synchronizing nginx config')
     self._config.WriteToFile(self._nginx_config_location)
     self._servers = self._parseConfig()
-    os.system('sudo systemctl reload nginx.service')
+    os.system('systemctl reload nginx.service')
+
+  def NotifyContainerHostDead(self, host):
+    self._servers.get(host).status = 'stopped'
+  
+  def NotifyContainerHostAlive(self, host):
+    self._servers.get(host).status = 'running'
+
 
 
 def main():
@@ -218,7 +317,7 @@ def main():
   app = api.GetFlask()
 
   # Setup the hosting mechanism
-  libhost.SetupContainerService(app, 'services.tedm.io')
+  libhost.SetupContainerService(app, 'hosts.tedm.io')
 
   # Setup the logger
   logs = logger.LogHost.AttachMemoryLogManager(app)
